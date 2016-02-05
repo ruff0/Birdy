e056d9effdb64d0e2473e64607aeb16a9d032b55
Veselin Tsvetanov <veselintsvetanov@gmail.com>
2016-02-05 22:35:03 +0200

    Adding podFiles

---
+++ Birdy2/Birdy2/LocationsViewController.swift
@@ -0,0 +1,56 @@
+//
+//  LocationsViewController.swift
+//  Birdy2
+//
+//  Created by veso on 2/5/16.
+//  Copyright © 2016 veso. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+
+@objc class LocationsViewController: UIViewController {
+    var locations: Array<Coordinates>!;
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let firstPointLat = (self.locations[0].latitude as NSString).doubleValue
+        let firstPointLon = (self.locations[0].longitude as NSString).doubleValue
+        
+        let camera = GMSCameraPosition.cameraWithLatitude(firstPointLat,
+            longitude: firstPointLon, zoom: 5)
+        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView.myLocationEnabled = true
+        self.view = mapView
+        
+        for point in self.locations {
+            let marker = GMSMarker()
+            let latitude = (point.latitude as NSString).doubleValue
+            let longitude = (point.longitude as NSString).doubleValue
+            marker.position = CLLocationCoordinate2DMake(latitude, longitude)
+            // marker.title = "Sydney"
+            // marker.snippet = "Australia"
+            marker.map = mapView
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
